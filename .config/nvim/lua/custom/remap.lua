5a3096fdd8ef9a6ecbfdc2752d89cdcead60fc57
Anton Kozyriev <sonara4mus@gmail.com>
2023-04-29 17:03:43 +0300

    feat: maps for git commands

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -1,3 +1,7 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>ga", ":!git add .<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gs", ":!git status<CR>")
+vim.keymap.set("n", "<leader>gd", ":!git diff<CR>")
+vim.keymap.set("n", "<leader>gds", ":!git diff --staged<CR>")
 
