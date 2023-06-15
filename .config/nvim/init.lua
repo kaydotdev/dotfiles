8cb520a1a5f415f9f32a931da803a6b0e29bf9a3
Anton Kozyriev <sonara4mus@gmail.com>
2023-06-16 00:27:25 +0300

    feat(init.lua): add code autoidentation commands

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,23 @@ require("custom")
 
 vim.opt.encoding = "utf-8"
 
-vim.cmd("set nofoldenable")
-vim.cmd("set relativenumber")
+-- Enable all mouse functions in all modes
+vim.cmd("set mouse=a")
+
+-- Visual settings
 vim.cmd("set guifont=firacode:h12")
+
+-- Line number settings
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
+-- Code identation settings
+vim.cmd("set autoindent")
+vim.cmd("set smarttab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+-- Prevent automatic code folding
+vim.cmd("set nofoldenable")
 
