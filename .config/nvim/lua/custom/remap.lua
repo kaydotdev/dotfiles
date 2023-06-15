53a0c2efd44d2ffcf608b8622ef8c223f102c0b5
Anton Kozyriev <sonara4mus@gmail.com>
2023-06-16 00:28:15 +0300

    fix: remove ambiguous git keymaps

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -3,5 +3,4 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ga", ":!git add .<CR>", { silent = true })
 vim.keymap.set("n", "<leader>gs", ":!git status<CR>")
 vim.keymap.set("n", "<leader>gd", ":!git diff<CR>")
-vim.keymap.set("n", "<leader>gds", ":!git diff --staged<CR>")
 
